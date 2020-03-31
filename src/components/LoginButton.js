9c014f859756178193bc2a2fd90bd9a62ebac134
Jack Lanser <jlanser2@illinois.edu>
2020-03-31 16:11:21 -0500

    Changed login button

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 // Example of css in js
 // Notice any similarities in the styles? If styles overlap, you can combine them!
@@ -19,50 +19,42 @@ import React from 'react';
 // Try it below!!
 
 const styles = {
-  onlineUserButton: {
-    display: 'flex',
-    width: '200px',
-    height: '30px',
-    backgroundColor: 'rgba(250, 202, 98, 0.849)',
-    border: 'none',
-    borderRadius: '5px',
-    textAlign: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontSize: '13px',
-    marginBottom: 10,
-  },
-  freeUserButton: {
-    display: 'flex',
-    width: '200px',
-    height: '30px',
-    backgroundColor: 'rgba(133, 250, 98, 0.769)',
-    border: 'none',
-    borderRadius: '5px',
-    textAlign: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontSize: '13px',
-    marginBottom: 10,
-  },
+  Button: {
+    display: "flex",
+    width: "200px",
+    height: "30px",
+    backgroundColor: "rgba(250, 202, 98, 0.849)",
+    border: "none",
+    borderRadius: "5px",
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: "13px",
+    marginBottom: 10
+  }
 };
 
-const onlineUserButton = (props) => {
+const onlineUserButton = props => {
   const { user } = props;
   return (
-    <div style={styles.onlineUserButton}>
+    <div style={styles.Button}>
       <p>{user.name}</p>
     </div>
   );
 };
 
 const freeUserButton = (
-  <div style={styles.freeUserButton}>
+  <div
+    style={{
+      ...styles.Button,
+      backgroundColor: "rgba(133, 250, 98, 0.769)"
+    }}
+  >
     <p> Log in now! </p>
   </div>
 );
 
-const LoginButton = (props) => {
+const LoginButton = props => {
   if (props.user.name) return onlineUserButton(props);
   return freeUserButton;
 };
