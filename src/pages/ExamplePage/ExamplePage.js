c17a54592a3f004eb373ae4bda32e9205749ce8c
Lam Tran <Lam7150@gmail.com>
2020-04-07 21:14:16 -0400

    Refactoring code (#16)
    Cleaned up and refactored components and created an ExamplePage for easy testing

---
+++ src/pages/ExamplePage/ExamplePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ExamplePage.css';
+
+function ExamplePage() {
+  return (
+    <div className="ExamplePage__container">
+      {/* Put whatever you want in here for easy testing! */}
+    </div>
+  );
+}
+
+export default ExamplePage;
--- src/screens/Home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Queue from './Queue';
-
-function Home() {
-  return <Queue />;
-}
-
-export default Home;
